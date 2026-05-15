8f99d83d6e8f342a1989e2fe2a5c28a3950ba1bf
S0LsTicee <SIQI.CHEN24@myhunter.cuny.edu>
2026-05-15 16:26:19 -0400

    Update Alpha_Beta_Diversity_Analysis.r

--- Alpha_Beta_Diversity_Analysis.r
+++ Alpha_Beta_Diversity_Analysis.r
@@ -5,6 +5,13 @@ my_packages <- c("phyloseq", "vegan", "ggplot2", "dplyr",
 
 install.packages(my_packages)
 
+# Check for missing files and install them
+not_installed <- my_packages[!(my_packages %in% installed.packages()[, "Package"])]
+
+if(length(not_installed)) {
+  install.packages(not_installed)
+}
+
 # Load merged files
 metaphlan <- read.delim(
   "metaphlan_merged.txt", #This is a placeholder for the name of your merged metaphlan file
